c9df9101b0a1fdf47143e820c0a70107b676d54f
Israfil Hossen <sisrafilss@gmail.com>
2024-08-28 15:40:35 +0600

    navigation routes added

--- index.js
+++ index.js
@@ -5,12 +5,24 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
-})
+  res.render("index.ejs");
+});
+
+app.get("/create-post", (req, res) => {
+  res.render("createPost.ejs");
+});
+
+app.get("/manage-blogs", (req, res) => {
+  res.render("manageBlog.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
 
 app.listen(port, () => {
-    console.log(`App running on port: ${port}`)
-})
+  console.log(`App running on port: ${port}`);
+});
